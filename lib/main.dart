e2ee11a9e913b576cfd56b3dc568636a8f58a60c
Dilesh Madhavan <madhavandilesh@gmail.com>
2023-08-25 18:40:16 +0530

    DB connection established

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:money_manager/models/category/category_model.dart';
 import 'package:money_manager/screen/home/screen_home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  if (Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
+  if (Hive.isAdapterRegistered(CategoryTypeAdapter().typeId)) {
+    Hive.registerAdapter(CategoryTypeAdapter());
+  }
   runApp(const MyApp());
 }
 
